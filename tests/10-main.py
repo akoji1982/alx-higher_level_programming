fcc0c8195a0d3cc3855876db4ba6af408b236eef
akoji@1982 <akoji1982@gmail.com>
2022-07-05 13:50:26 -0700

    akoji

--- tests/10-main.py
+++ tests/10-main.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-Square = __import__('10-square').Square
+Student = __import__('10-student').Student
 
-s = Square(13)
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
 
-print(s)
-print(s.area())
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
